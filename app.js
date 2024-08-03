2bca35ba01ee24c4f61f257dbfdaf06fdca2005f
Ramadan-Botsher <ramadanbotsher@gmail.com>
2024-08-03 15:00:19 +0200

    using mongodb

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose")
 
 app.get("/" , (req,res) => {
     res.sendFile("./views/home.html",{root: __dirname});
 })
 
-app.listen(port, () => {
-    console.log(`http://localhost:${port}/`);
-})
+
+
+mongoose.connect('mongodb+srv://ramadanbotsher:cBO1lKsBIttxy39u@botsher.ewvrxnf.mongodb.net/all-data?retryWrites=true&w=majority&appName=botsher').then(() => {
+    app.listen(port, () => {
+        console.log(`http://localhost:${port}/`);
+    })
+  }).catch((err) => {console.log(err)});
